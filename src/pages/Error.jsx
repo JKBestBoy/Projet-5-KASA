27e03f180fa4cdec3cafd8fb347de6b6a7dbc6c5
JK <bleach.06@live.fr>
2024-04-19 00:18:34 +0200

    Error page

---
+++ src/pages/Error.jsx
@@ -0,0 +1,23 @@
+import { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import Header from '../components/Header.jsx';
+import Footer from '../components/Footer.jsx';
+import Home from '../pages/Home.jsx';
+import './Erreur.scss'
+
+export default function Erreur() {
+    return (
+      <Fragment>
+        <Header />
+        <main className='main'>
+        <h1>404</h1>
+        <h2>Oups! La page que vous demandez n'existe pas.</h2>
+        <Link to="/"  className='link'>
+            <p>Retourner à la page d'accueil</p>
+        </Link>
+        </main>
+        <Footer />
+      </Fragment>
+    );
+  }
+  
